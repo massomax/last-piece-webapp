fca3d1528a0648253268bc6d0fbe4fca415e1d0c
massomax <massocode@gmail.com>
2025-06-06 20:19:10 +0300

    Добвлены товары

--- src/mocks.ts
+++ src/mocks.ts
@@ -44,4 +44,24 @@ export const mockProducts: Product[] = [
     sellerUsername: "kitchen_master",
     createdAt: new Date().toISOString(),
   },
+  {
+    _id: "5",
+    title: "Смартфон Xiaomi Redmi Note 9",
+    description: "Полностью рабочий, без зарядника.",
+    price: 4990,
+    imageUrl:
+      "https://avatars.mds.yandex.net/get-mpic/4590616/img_id1764329017944712732.jpeg/orig",
+    sellerUsername: "xiaomi_seller",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    _id: "6",
+    title: "Кухонная вытяжка Electrolux",
+    description: "Мощная, почти новая. Гарантия 6 месяцев.",
+    price: 5990,
+    imageUrl:
+      "https://avatars.mds.yandex.net/get-mpic/1924580/img_id8653184539956473429.jpeg/optimize",
+    sellerUsername: "kitchen_master",
+    createdAt: new Date().toISOString(),
+  },
 ];
